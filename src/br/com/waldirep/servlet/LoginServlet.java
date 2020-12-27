a9de1b6c3c043f8e867952426a3538ad34868025
wep1980 <wepbike@gmail.com>
2020-12-27 08:29:12 -0300

    Login servlet

--- src/br/com/waldirep/servlet/LoginServlet.java
+++ src/br/com/waldirep/servlet/LoginServlet.java
@@ -10,52 +10,49 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import br.com.waldirep.DAO.LoginDAO;
+import br.com.waldirep.util.LogUtil;
 
 /**
- * Classe servlet que intercepta os dados
+ * Classe servlet que intercepta os dados do login
  */
 @WebServlet("/LoginServlet")
 public class LoginServlet extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-   
-	
-	private LoginDAO loginDAO = new LoginDAO();
-	
-    public LoginServlet() {
-        super();
-      
-    }
 
-    
-    /**
-     * Intercepta o formulario pegando pela URL os dados
-     */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
-		doPost(request, response);
-		
+	private static final long serialVersionUID = 1L;
+
+	private final LoginDAO loginDAO = new LoginDAO();
+
+	public LoginServlet() {
+		super();
 	}
 	
-
+	
 	/**
 	 * Intercepta o envio do formulario
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		 
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
 		try {
-			
-		 String login = request.getParameter("login"); //Pega o login digitado na frontend
-		 String senha = request.getParameter("senha"); //Pega a senha digitado na frontend
-		 
-		 if(loginDAO.validarLogin(login, senha)) {
-			 RequestDispatcher dispatcher = request.getRequestDispatcher("acessoLiberado.jsp");
-			 dispatcher.forward(request, response);
-		 }else {
-			 RequestDispatcher dispatcher = request.getRequestDispatcher("acessoNegado.jsp");
-			 dispatcher.forward(request, response);
-		 } 
-		 } catch (Exception e) {
-			
+			String login = request.getParameter("login"); //Pega o login digitado na frontend
+			String senha = request.getParameter("senha"); //Pega a senha digitado na frontend
+
+			if (login != null && !login.isEmpty() && senha != null && !senha.isEmpty()) {
+				if (loginDAO.validarLogin(login, senha)) {
+					RequestDispatcher requestDispatcher = request.getRequestDispatcher("acessoliberado.jsp");
+					requestDispatcher.forward(request, response);
+				} else {
+					RequestDispatcher requestDispatcher = request.getRequestDispatcher("acessonegado.jsp");
+					requestDispatcher.forward(request, response);
+				}
+			} else {
+				RequestDispatcher requestDispatcher = request.getRequestDispatcher("index.jsp");
+				requestDispatcher.forward(request, response);
 			}
+		} catch (Exception e) {
+			LogUtil.getLogger(LoginServlet.class).error(e.getCause().toString());
+		}
 	}
+
 }
