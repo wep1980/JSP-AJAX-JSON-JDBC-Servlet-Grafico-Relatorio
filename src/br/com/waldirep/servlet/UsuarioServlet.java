c5818c5a300a59b3d79473ca220ca0d0743e4f32
wep1980 <wepbike@gmail.com>
2020-12-27 08:25:55 -0300

    Usuario servlet

--- src/br/com/waldirep/servlet/UsuarioServlet.java
+++ src/br/com/waldirep/servlet/UsuarioServlet.java
@@ -2,131 +2,213 @@ package br.com.waldirep.servlet;
 
 import java.io.IOException;
 
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.MultipartConfig;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import br.com.waldirep.DAO.UsuarioDAO;
 import br.com.waldirep.beans.Usuario;
+import br.com.waldirep.service.UsuarioService;
+import br.com.waldirep.util.LogUtil;
 
 @WebServlet("/salvarUsuario")
+@MultipartConfig
 public class UsuarioServlet extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-
-	private UsuarioDAO daoUsuario = new UsuarioDAO();
-
-	public UsuarioServlet() {
-		super();
-
-	}
-
-	protected void doGet(HttpServletRequest request,
-			HttpServletResponse response) throws ServletException, IOException {
-		try {
-			String acao = request.getParameter("acao");
-			String user = request.getParameter("user");
-
-			if (acao.equalsIgnoreCase("delete")) {
-				daoUsuario.deletar(user);
-				RequestDispatcher view = request
-						.getRequestDispatcher("/cadastroUsuario.jsp");
-				request.setAttribute("usuarios", daoUsuario.listarTodos());
-				view.forward(request, response);
-			} else if (acao.equalsIgnoreCase("editar")) {
-
-				Usuario usuario = daoUsuario.consultar(user);
-
-				RequestDispatcher view = request
-						.getRequestDispatcher("/cadastroUsuario.jsp");
-				request.setAttribute("user", usuario);
-				view.forward(request, response);
-			} else if (acao.equalsIgnoreCase("listartodos")) {
-
-				RequestDispatcher view = request
-						.getRequestDispatcher("/cadastroUsuario.jsp");
-				request.setAttribute("usuarios", daoUsuario.listarTodos());
-				view.forward(request, response);
-			}
-
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
-
-	protected void doPost(HttpServletRequest request,
-			HttpServletResponse response) throws ServletException, IOException {
-
+	
+	private static final long serialVersionUID = 1L; 
+	
+	private static final UsuarioService usuarioService = new UsuarioService();
+   
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		
 		String acao = request.getParameter("acao");
-
+		String user = request.getParameter("user");
+		
+		executarAcao(acao, user, request, response);
+	}	
+	
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {		
+		
+		String acao = request.getParameter("acao");
+		
+		String[] atributosUsuario = {"id", "login", "senha", "nome", "cep", "rua", "bairro", "cidade", "estado", "ibge", "ativo", "sexo", "perfil"};
+		
 		if (acao != null && acao.equalsIgnoreCase("reset")) {
-			try {
-				RequestDispatcher view = request
-						.getRequestDispatcher("/cadastroUsuario.jsp");
-				request.setAttribute("usuarios", daoUsuario.listarTodos());
-				view.forward(request, response);
+			listarTodosUsuariosRedirecionar(request, response);				
+		} else {				
+			Usuario usuario = usuarioService.criarUsuario(request, atributosUsuario);			
 
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-		} else {
-
-			String id = request.getParameter("id");
-			String login = request.getParameter("login");
-			String senha = request.getParameter("senha");
-			String nome = request.getParameter("nome");
-
-			Usuario usuario = new Usuario();
-			usuario.setId(!id.isEmpty() ? Long.parseLong(id) : null);
-			usuario.setLogin(login);
-			usuario.setSenha(senha);
-			usuario.setNome(nome);
-			try {
-
-				String msg = null;
-				boolean podeInserir = true;
-
-				if (id == null || id.isEmpty()
-						&& !daoUsuario.validarLogin(login)) {//QUANDO DOR USUÁRIO NOVO
-					msg = "Usuário já existe com o mesmo login!";
-					podeInserir = false;
-
-				} else if (id == null || id.isEmpty()
-						&& !daoUsuario.validarSenha(senha)) {// QUANDO FOR USUÁRIO NOVO
-					msg = "\n A senha já existe para outro usuário!";
-					podeInserir = false;
-				}
-
-				if (msg != null) {
-					request.setAttribute("msg", msg);
-				}
-
-				if (id == null || id.isEmpty()
-						&& daoUsuario.validarLogin(login) && podeInserir) {
-
-					daoUsuario.salvar(usuario);
-
-				} else if (id != null && !id.isEmpty() && podeInserir) {
-					daoUsuario.atualizar(usuario);
+			usuarioService.validarCamposFormulario(atributosUsuario, request, response, usuario);
+			
+			boolean loginExiste = false;
+			boolean senhaExiste = false;
+			
+			loginExiste = validarLoginInsert(getValue(request, atributosUsuario[0]), 
+					getValue(request, atributosUsuario[1]));
+			
+			senhaExiste = validarSenhaInsert(getValue(request, atributosUsuario[0]), 
+					getValue(request, atributosUsuario[2]));
+			
+			request.setAttribute("msg", usuarioService.definirMensagem(loginExiste, senhaExiste));
+			request.setAttribute("user", usuario);			
+			
+			if (getValue(request, atributosUsuario[0]) == null 
+					|| getValue(request, atributosUsuario[0]).isEmpty() 
+					&& validarLoginInsert(getValue(request, atributosUsuario[1])) 
+					&& validarSenhaInsert(getValue(request, atributosUsuario[2]))) {
+				
+				enviarImagem(request, usuario);
+				enviarCurriculo(request, usuario);
+				
+				salvar(usuario);
+				
+				request.setAttribute("user", null);
+				request.setAttribute("msgSalvarAtualizar", "Usu�rio cadastrado com sucesso!");
+				
+			} else if (getValue(request, atributosUsuario[0]) != null 
+					&& !getValue(request, atributosUsuario[0]).isEmpty()) {
+				
+				loginExiste = validarLoginUpdate(getValue(request, atributosUsuario[0]),
+						getValue(request, atributosUsuario[1]));
+				
+				senhaExiste = validarSenhaUpdate(getValue(request, atributosUsuario[0]),
+						getValue(request, atributosUsuario[2]));
+				
+				boolean atualizou = false;
+				
+				if (!loginExiste && !senhaExiste) {
+					atualizou = true;
+					enviarImagem(request, usuario);
+					enviarCurriculo(request, usuario);
+					atualizar(usuario);	
+					request.setAttribute("msgSalvarAtualizar", "Usu�rio atualizado com sucesso!");
 				}
 				
-				if (!podeInserir){
+				if (!atualizou) { 
+					request.setAttribute("msg", usuarioService.definirMensagem(loginExiste, senhaExiste));
 					request.setAttribute("user", usuario);
+				} else {
+					request.setAttribute("user", null);
 				}
-
-				RequestDispatcher view = request
-						.getRequestDispatcher("/cadastroUsuario.jsp");
-				request.setAttribute("usuarios", daoUsuario.listarTodos());
-				view.forward(request, response);
-
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-
+			}				
+			listarTodosUsuariosRedirecionar(request, response);
+		}	
+	}	
+	
+	public void salvar(Usuario usuario) {
+		try {
+			usuarioService.salvar(usuario);
+		} catch (Exception e) {
+			LogUtil.getLogger(UsuarioServlet.class).error(e.getCause().toString());
 		}
+	}	
+	
+	private void atualizar(Usuario usuario) {
+		try {
+			usuarioService.atualizar(usuario);
+		} catch (Exception e) {
+			LogUtil.getLogger(UsuarioServlet.class).error(e.getCause().toString());
+		}
+	}	
+	
+	private void executarAcao(String acao, String user, HttpServletRequest request, HttpServletResponse response) {
+		try {
+			usuarioService.executarAcao(acao, user, request, response);	
+		} catch (Exception e) {
+			LogUtil.getLogger(UsuarioServlet.class).error(e.getCause().toString());
+		}
+	}	
+	
+	private void listarTodosUsuariosRedirecionar(HttpServletRequest request, HttpServletResponse response) {	
+		try {
+			usuarioService.listarTodosUsuariosRedirecionar(request, response);
+		} catch (Exception e) {
+			LogUtil.getLogger(UsuarioServlet.class).error(e.getCause().toString());
+		}			
+	}		
+	
+	private void enviarImagem(HttpServletRequest request, Usuario usuario) {
+		try {
+			usuarioService.enviarImagem(request, usuario);
+		} catch (Exception e) {
+			LogUtil.getLogger(UsuarioServlet.class).error(e.getCause().toString());
+		}
+	}
+	
+	private void enviarCurriculo(HttpServletRequest request, Usuario usuario) {
+		try {
+			usuarioService.enviarCurriculo(request, usuario);
+		} catch (Exception e) {
+			LogUtil.getLogger(UsuarioServlet.class).error(e.getCause().toString());
+		}
+	}
+	
+	private String getValue(HttpServletRequest request, String valor) {
+		return usuarioService.getValue(request, valor);
+	}
+	
+	private boolean validarLoginInsert(String id, String login) {
+		Boolean loginValido = Boolean.FALSE;
+		try {
+			loginValido = usuarioService.validarLoginInsert(id, login);			
+		} catch (Exception e) {
+			LogUtil.getLogger(UsuarioServlet.class).error(e.getCause().toString());
+		}
+		return loginValido;
+	}
 
+	private boolean validarLoginInsert(String login) {		
+		Boolean loginValido = Boolean.FALSE;
+		try {
+			loginValido = usuarioService.validarLoginInsert(login);			
+		} catch (Exception e) {
+			LogUtil.getLogger(UsuarioServlet.class).error(e.getCause().toString());
+		}
+		return loginValido;		
+	}
+
+	private boolean validarSenhaInsert(String id, String senha) {
+		Boolean loginValido = Boolean.FALSE;
+		try {
+			loginValido = usuarioService.validarSenhaInsert(id, senha);
+		} catch (Exception e) {
+			LogUtil.getLogger(UsuarioServlet.class).error(e.getCause().toString());
+		}
+		return loginValido;		}
+
+	private boolean validarSenhaInsert(String senha) {
+		Boolean loginValido = Boolean.FALSE;
+		try {
+			loginValido = usuarioService.validarSenhaInsert(senha);
+		} catch (Exception e) {
+			LogUtil.getLogger(UsuarioServlet.class).error(e.getCause().toString());
+		}
+		return loginValido;	
+	}
+
+	private boolean validarLoginUpdate(String id, String login) {
+		Boolean loginValido = Boolean.FALSE;
+		try {
+			loginValido = usuarioService.validarLoginUpdate(id, login);
+		} catch (Exception e) {
+			LogUtil.getLogger(UsuarioServlet.class).error(e.getCause().toString());
+		}
+		return loginValido;	
+	}
+
+	private boolean validarSenhaUpdate(String id, String senha) {
+		Boolean loginValido = Boolean.FALSE;
+		try {
+			loginValido = usuarioService.validarSenhaUpdate(id, senha);			
+		} catch (Exception e) {
+			LogUtil.getLogger(UsuarioServlet.class).error(e.getCause().toString());
+		}
+		return loginValido;	
 	}
 
 }
