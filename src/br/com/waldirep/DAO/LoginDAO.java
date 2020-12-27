0d159e41542c45aeaeed2746be6ffc0b343bbb75
wep1980 <wepbike@gmail.com>
2020-12-27 08:09:43 -0300

    Login DAO

--- src/br/com/waldirep/DAO/LoginDAO.java
+++ src/br/com/waldirep/DAO/LoginDAO.java
@@ -3,32 +3,25 @@ package br.com.waldirep.DAO;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 
 import br.com.waldirep.connection.SingleConnection;
 
 public class LoginDAO {
 	
-	private static Connection connection;
-	
+	private Connection connection;
 	
 	public LoginDAO() {
 		connection = SingleConnection.getConnection();
 	}
 	
-	
-	
-	public boolean validarLogin(String login, String senha) throws Exception{
-		
-		String sql = "SELECT * FROM usuario where login = '"+login+"' and senha = '"+senha+"'";
-		
-		PreparedStatement statement = connection.prepareStatement(sql); 
-		ResultSet result = statement.executeQuery(); // Carrega o resultado do SQL
-		
-		if(result.next()) {
-			return true; // Validou o usuario
-		}else {
-			return false; // Não validou o usuario
-		}
-	}
+	public boolean validarLogin(String login, String senha) throws SQLException {		
+		String sql = "SELECT * FROM usuario WHERE login = '" + login + "' AND senha = '" + senha + "'";
+		try (PreparedStatement statement = connection.prepareStatement(sql);) {
+			try (ResultSet result = statement.executeQuery()) {
+				return result.next() ? Boolean.TRUE : Boolean.FALSE;
+			}			
+		}			
+	}	
 
 }
